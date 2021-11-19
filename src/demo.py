3a1459a8e788d9e623362daf246608b39a54fde6
joshuasia <joshsia@student.ubc.ca>
2021-11-18 22:36:43 -0800

    Add python script which prints docopt arguments

---
+++ src/demo.py
@@ -0,0 +1,17 @@
+# author: Joshua Sia
+# date: 2021-11-18
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
+Options:
+<arg1>             Takes any value (this is a required positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+[<arg4>]
+""" 
+
+from docopt import docopt
+opt = docopt(__doc__)
+print(opt["<arg4>"])
+print(opt)
+print(type(opt))
